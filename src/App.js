5ac779eaaebc9e27ea71670b6e40662b31557b2c
Julian <julianmartinez_93@hotmail.com>
2022-11-21 23:10:19 -0300

    delete folder and add admin folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Home from './components/Home/Home'
 import { Switch, Route } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
+import Admin from './components/Admin/Admin'
 
 function App () {
   return (
     <div className=' container-fluid ms-0 me-0 ps-0 pe-0 App '>
-      <Nav />
+      <Route exact path={['/']} component={Nav} /> 
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path={'/admin'} component={Admin}></Route>
       </Switch>
     </div>
   )
