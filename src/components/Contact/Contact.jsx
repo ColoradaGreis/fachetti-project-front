0fd6c557281d20bb89bd8ffc6886b1d24afc50be
LHC <lautarohconti@gmail.com>
2022-11-26 12:24:45 -0300

    Formik modularizado

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,52 +1,48 @@
 import React from 'react'
-import { Form, Formik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import s from './Contact.module.css'
+
+const initialValues = {
+  fullName: '',
+  phone: '',
+  email: '',
+  message: ''
+}
+const validateFields = values => {
+  const errors = {}
+  if (!values.fullName) errors.fullName = 'Campo requerido'
+  if (!values.phone) errors.fullName = 'Campo requerido'
+  if (!values.email) errors.fullName = 'Campo requerido'
+  if (!values.message) errors.fullName = 'Campo requerido'
+  return errors
+}
+const handleSubmit = (values, { setFieldError }) => {
+  return register(values)
+    .catch(() => { setFieldError('fullName', 'This fullName is not valid') })
+}
 export default function Contact () {
   return (
     <div>
       <h1>Contáctanos</h1>
       <h4>Ecríbenos y en breve nos pondremos en contacto contigo</h4>
       <Formik
-        initialValues={{
-          fullName: '',
-          phone: '',
-          email: '',
-          message: ''
-        }}
-        validate={values => {
-          const errors = {}
-          if (!values.fullName) errors.fullName = 'Campo requerido'
-          if (!values.phone) errors.fullName = 'Campo requerido'
-          if (!values.email) errors.fullName = 'Campo requerido'
-          if (!values.message) errors.fullName = 'Campo requerido'
-        }}
-        onSubmit={(values, { setFieldError }) => {
-          return register(values)
-            .catch(() => { setFieldError('fullName', 'This fullName is not valid') })
-        }}
+        initialValues={initialValues}
+        validate={validateFields}
+        onSubmit={handleSubmit}
       >
         {
-          ({ handleSubmit, handleChange, isSubmitting, errors }) => (
-            <form onSubmit={handleSubmit}>
-              <input onChange={handleChange} name='fullName' placeholder='Ingrese su nombre completo' />
-              {errors.fullName && (
-                <small style={{ color: 'red' }}>
-                  {errors.fullName ? <p>{errors.fullName}</p> : ''}
-                </small>)}
-              <input onChange={handleChange} name='phone' placeholder='ingrese su número de teléfono' />
-              <small style={{ color: 'red' }}>
-                {errors.phone ? <p>{errors.phone}</p> : ''}
-              </small>
-              <input onChange={handleChange} name='email' placeholder='Ingrese su email' />
-              <small style={{ color: 'red' }}>
-                {errors.email ? <p>{errors.email}</p> : ''}
-              </small>
-              <input onChange={handleChange} name='message' placeholder='Cuál es su consulta?' />
-              <small style={{ color: 'red' }}>
-                {errors.message ? <p>{errors.message}</p> : ''}
-              </small>
+          ({ isSubmitting, errors }) => (
+            <Form>
+              <Field name='fullName' placeholder='Ingrese su nombre completo' />
+              <ErrorMessage name='fullname' component='small' />
+              <Field name='phone' placeholder='ingrese su número de teléfono' />
+              <ErrorMessage name='phone' component='small' />
+              <Field name='email' placeholder='Ingrese su email' />
+              <ErrorMessage name='email' component='small' />
+              <Field name='message' placeholder='Cuál es su consulta?' />
+              <ErrorMessage name='message' component='small' />
               <button className={s.btn} disabled={isSubmitting}>Enviar</button>
-            </form>)
+            </Form>)
         }
       </Formik>
     </div>
