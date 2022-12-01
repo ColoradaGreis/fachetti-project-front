7dc78e35633380e1539f2d08bac391d3c259fc8c
Julian <julianmartinez_93@hotmail.com>
2022-12-01 20:43:28 -0300

    add lazi loading

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,22 @@
+import { lazy, Suspense } from 'react' // eslint-disable-line
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Guard from './Guard'
-import PrivateRoutes from './PrivateRoutes'
-import PublicRoutes from './PublicRoutes'
+import Loading from '../components/Loading/Loading'
+
+const PublicRoutesLazy = lazy(() => import('./PublicRoutes'))
+const PrivateRoutesLazy = lazy(() => import('./PrivateRoutes'))
 
 export default function AppRoutes () {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<PublicRoutes />} />
-        <Route element={<Guard />}>
-          <Route path='/private/*' element={<PrivateRoutes />} />
-        </Route>
-
-      </Routes>
-
-    </BrowserRouter>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<PublicRoutesLazy />} />
+          <Route element={<Guard />}>
+            <Route path='/private/*' element={<PrivateRoutesLazy />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
