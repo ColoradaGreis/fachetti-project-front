bdbc44cf7f7186055b836073454080993dba67a7
Julian <julianmartinez_93@hotmail.com>
2022-12-15 18:14:08 -0300

    add accrodion

---
+++ src/data/fakerConsultsData.js
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+// import { faker } from '@faker-js/faker/locale/de';
+
+export const Consults = []
+const cellPhoneArray = [
+  faker.phone.number(),
+  undefined
+]
+
+function createRandomUser () {
+  return {
+    id: faker.datatype.uuid(),
+    name: faker.name.fullName(),
+    email: faker.internet.email(),
+    phone: faker.helpers.arrayElement(cellPhoneArray),
+    message: faker.lorem.paragraph(),
+    date: faker.date.recent(),
+    read: faker.datatype.boolean()
+  }
+}
+
+Array.from({ length: 20 }).forEach(() => {
+  Consults.push(createRandomUser())
+})
--- src/private/pages/Consults/Consults.jsx
+++ src/private/pages/Consults/Consults.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import { Consults as data } from '../../../data'
+import Body from './Body'
 
 export default function Consults () {
-  return (
-    <div>
-      Consults
+  const [state, setState] = useState([])// [booleanos]
 
-    </div>
+  const isRead = (id) => {
+    if (id === null) return
+    const index = data.findIndex(user => user.id === id)
+    if (state[index]) return
+    setState(prev => {
+      const newState = [...prev]
+      newState[index] = true
+      return newState
+    })
+  }
+  useEffect(() => {
+    const readState = data.map(user => user.read)
+    setState(readState)
+  }, [data])
+  console.log(data)
+  return (
+    <>
+      {
+      data.map((user, index) => (
+        <Accordion defaultActiveKey='0' key={index} onSelect={isRead}>
+          <Accordion.Item eventKey={user.id}>
+            <Accordion.Header>{user.name}</Accordion.Header>
+            <Accordion.Body>
+              <Body user={user} read={state[index]} />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      ))
+    }
+    </>
   )
 }
