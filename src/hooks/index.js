810f4acaf061106c9558ff684fa09a1cef2cd623
ColoradaGreis <graciana.baratti@gmail.com>
2022-12-19 20:03:05 -0300

    producto por categoría

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,2 @@
 export { default as useGetAllCategories } from './useGetAllCategories'
+export { default as useGetAllProducts } from './useGetAllProducts'
--- src/public/components/Products/CategoryProducts.jsx
+++ src/public/components/Products/CategoryProducts.jsx
@@ -1,32 +1,41 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { getProductsByCategories } from '../../../redux/products/productsActions'
+import React from 'react'
+// import { useSelector } from 'react-redux'
 import Card from '../Card/Card'
+import s from './Product.module.css'
+import { useGetAllProducts } from '../../../hooks'
+import Loading from '../Loading/Loading.jsx'
+import { Link, useParams } from 'react-router-dom'
 
-export default function CategoryProduct () {
-  const products = useSelector(state => state.products)
-  const dispatch = useDispatch()
-  const params = useParams()
+export default function CategoryProducts () {
+  const { category } = useParams()
+  const { data, loading, error } = useGetAllProducts(category)
+  console.log('esta es la data que me llega', data)
+  console.log(error)
 
-  useEffect(() => {
-    dispatch(getProductsByCategories(params.name))
-  })
   return (
     <div>
-      <h1>Nuestros Productos</h1>
-      <h3>{params.name}</h3>
-      {
-        Array.isArray(products) && products.map(p => (
-          <Card
-            key={p.id}
-            id={p.id}
-            name={p.name}
-            description={p.description}
-            image={p.image}
-          />
-        ))
-      }
+      <h1 className={s.h1Products}>Acá hay sólo productos de una categoría</h1>
+      <h3 className={s.h3Products}>Nuestros productos</h3>
+      <div className='d-flex flex-wrap mx-5 p-5 gap-5 justify-content-center'>
+
+        {
+        loading
+          ? <Loading />
+          : error
+            ? console.log(error) //eslint-disable-line
+            : data.map(e =>
+              <Link key={e.id} to={`categories/detail/${e.id}`}>
+                <Card
+                  key={e.id}
+                  id={e.id}
+                  title={e.name}
+                  image={e.image}
+                />
+              </Link>
+            )
+
+        }
+      </div>
     </div>
   )
 }
