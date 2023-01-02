f32f549963ecf0e6884d8765cb1e64304d97aca8
Julian <julianmartinez_93@hotmail.com>
2023-01-02 16:13:05 -0300

    add autorize at axios

--- src/api/API.js
+++ src/api/API.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
+import { getToken } from '../public/utils'
 
 // export const urlApi = axios.create({
 //   baseURL: 'https://fachettiprojectback-production.up.railway.app/'
 // })
+const token = getToken() || ''
+const tokenHeader = token ? `Bearer ${token}` : ''
+
 export const urlApi = axios.create({
-  baseURL: 'http://localhost:3002/'
+  baseURL: 'http://localhost:3002/',
+  headers: {
+    authorization: tokenHeader
+  }
+
 })
 export const localhost = axios.create({
   baseURL: 'https://localhost:3000/'
