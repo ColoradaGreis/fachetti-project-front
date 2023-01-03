2ed0ce382512e5bde7eb478bd8550958bc40346d
ColoradaGreis <graciana.baratti@gmail.com>
2023-01-03 17:21:24 -0300

    Publications

--- src/public/components/News/News.jsx
+++ src/public/components/News/News.jsx
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
-import { getAllPublications } from '@/redux/publications/publicationSlice'
+import React from 'react'
 import { Card } from '../Card'
 import s from './News.module.css'
+import { useGetAllPublications } from '../../../hooks'
+import Loading from '../Loading/Loading.jsx'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 export default function News () {
-  const dispatch = useDispatch()
-  const news = useSelector(state => state.publications)
+  const { t } = useTranslation('public')
+  const { data, loading, error } = useGetAllPublications()
 
-  useEffect(() => {
-    dispatch(getAllPublications())
-  }, [])
   return (
     <div>
-      <h1 className={s.h1News}>Novedades</h1>
-      {
-        Array.isArray(news) && news.map(n => (
-          <Navigate key={n.id} link={n.id}>
-            <Card
-              key={n.id}
-              id={n.id}
-              title={n.name}
-              image={n.image}
-              description={n.description}
-            />
-          </Navigate>
-        ))
-            }
+      <h1 className={s.h1Products}>{t('Publications.title')}</h1>
+      <h3 className={s.h3Products}>{t('Publications.subTitle')}</h3>
+      <div className='d-flex flex-wrap mx-5 p-5 gap-5 justify-content-center'>
+
+        {
+        loading
+          ? <Loading />
+          : error
+            ? alert(error) //eslint-disable-line
+            : data.map(e =>
+              <Link key={e.id} to={`/publications/${e.id}`}>
+                <Card
+                  key={e.id}
+                  id={e.id}
+                  title={e.title}
+                  image={e.image}
+                />
+              </Link>
+            )
+
+        }
+      </div>
     </div>
   )
 }
