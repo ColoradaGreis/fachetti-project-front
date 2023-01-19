3e413d0b188d2e4fa102605745e8f0e3d78146b4
Julian <julianmartinez_93@hotmail.com>
2023-01-19 13:08:08 -0300

    udpate routes public

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,26 +1,24 @@
 import { Route } from 'react-router-dom'
 import { PublicNameRoutes } from './routes.name'
 import NotFoundRoute from './NotFoundRoute'
-import { Home, Clients, Products, News, Contact, Footer, Nav, CategoryProducts, DetailProduct } from '../public/components'
+import { Home, Clients, Products, News, Contact, Nav, CategoryProducts, DetailProduct } from '../public/components'
 import { LoginAdmin } from '../public/pages'
 
 export default function PublicRoutes () {
   return (
     <>
-      <Nav />
-
       <NotFoundRoute>
-
-        <Route index path={PublicNameRoutes.HOME} element={<Home />} />
-        <Route path={PublicNameRoutes.CLIENTS} element={<Clients />} />
-        <Route path={PublicNameRoutes.CATEGORIES} element={<Products />} />
-        <Route path={PublicNameRoutes.PRODUCTS} element={<CategoryProducts />} />
-        <Route path={PublicNameRoutes.DETAIL} element={<DetailProduct />} />
-        <Route path={PublicNameRoutes.NEWS} element={<News />} />
-        <Route path={PublicNameRoutes.CONTACT} element={<Contact />} />
-        <Route path={PublicNameRoutes.LOGIN} element={<LoginAdmin />} />
+        <Route element={<Nav />}>
+          <Route index path={PublicNameRoutes.HOME} element={<Home />} />
+          <Route path={PublicNameRoutes.CLIENTS} element={<Clients />} />
+          <Route path={PublicNameRoutes.CATEGORIES} element={<Products />} />
+          <Route path={PublicNameRoutes.PRODUCTS} element={<CategoryProducts />} />
+          <Route path={PublicNameRoutes.DETAIL} element={<DetailProduct />} />
+          <Route path={PublicNameRoutes.NEWS} element={<News />} />
+          <Route path={PublicNameRoutes.CONTACT} element={<Contact />} />
+          <Route path={PublicNameRoutes.LOGIN} element={<LoginAdmin />} />
+        </Route>
       </NotFoundRoute>
-      <Footer />
     </>
   )
 }
