13bc5e1d4c810c026859e2e7b78cae696058072d
Julian <julianmartinez_93@hotmail.com>
2023-01-23 22:10:59 -0300

    new configurate i18next

--- src/config/i18next-config.js
+++ src/config/i18next-config.js
@@ -1,19 +1,24 @@
 import i18next from 'i18next'
-import Backend from 'i18next-http-backend'
+import ChainedBackend from 'i18next-chained-backend'
 import Languagedetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import HttpBackend from 'i18next-http-backend'
 
 i18next
-  .use(Backend)
+  .use(ChainedBackend)
   .use(Languagedetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
+    // ... your i18next config
     backend: {
-      // for all available options read the backend's repository readme file
-      loadPath: 'http://127.0.0.1:5173/locales/{{lng}}/{{ns}}.json'
-    },
-    interpolation: {
-      escapeValue: false // react already safes from xss
+      backends: [
+        HttpBackend,
+        resourcesToBackend((lng, ns) => import(`./locales/${lng}/${ns}.json`))
+      ],
+      backendOptions: [{
+        loadPath: '/locales/{{lng}}/{{ns}}.json'
+      }]
     }
   })
